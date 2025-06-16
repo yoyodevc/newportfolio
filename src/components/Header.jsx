9112bfe4d35796773a30095a8c05fd6c907f1143
yoyodev <jfredlim1303@gmail.com>
2025-06-17 00:30:28 +0800

    fixed horizontal scroll bar and updated resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import { FiDownload } from 'react-icons/fi';
 
 const Header = () => {
   return (
-    <header className="w-full px-6 py-4 flex justify-between items-center text-white font-[Poppins] [@media(min-width:1500px)]:sticky top-0 z-50">
+    <header className="w-full px-7 py-4 flex justify-between items-center text-white font-[Poppins] [@media(min-width:1500px)]:sticky top-0 z-50">
       <a href="#" className="text-2xl font-semibold text-gray-300 hover:text-lime-400 transition duration-300">
         Yoyodevc
       </a>
       <div className="relative group">
         <a
-          href="/JFL Resume.pdf"
+          href="/Resume - John Fredrick Lim.pdf"
           download
           aria-label="Download my CV"
           className="text-white hover:text-lime-400 transition duration-300"
